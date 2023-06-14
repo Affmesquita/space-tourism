02271ac3a14b2442950235ed4d607ffaa4ee73a7
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2023-06-14 17:25:05 -0300

    atualizado

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,5 +4,15 @@ function showHide(id) {
 }
 
 function mudar() {
-    let 
+    let crewImg = document.getElementsByClassName('crew-img');
+    let radio1 = document.getElementById('radio-1');
+    let radio2 = document.getElementById('radio-2');
+    let radio3 = document.getElementById('radio-3');
+    let radio4 = document.getElementById('radio-4');
+
+    if (radio1.checked) {
+        crewImg.setAttribute('src', 'image-anousheh-ansari.png')
+    } else if (radio1.checked) {
+        crewImg.setAttribute('src', 'image-douglas-hurley.png')
+    }
 }
