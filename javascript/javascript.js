f4b0a95c3ea505f748346255ee00a6c00c260c00
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2023-06-19 16:50:57 -0300

    atualizado JS

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,18 +1,12 @@
-function showHide(id) {
-    let conteudo = document.querySelector('id');
-    conteudo.classList.toggle('ativo')
-}
+var boxes = document.getElementsByClassName("slide-descrition");
 
-function mudar() {
-    let crewImg = document.getElementsByClassName('crew-img');
-    let radio1 = document.getElementById('radio-1');
-    let radio2 = document.getElementById('radio-2');
-    let radio3 = document.getElementById('radio-3');
-    let radio4 = document.getElementById('radio-4');
-
-    if (radio1.checked) {
-        crewImg.setAttribute('src', 'image-anousheh-ansari.png')
-    } else if (radio1.checked) {
-        crewImg.setAttribute('src', 'image-douglas-hurley.png')
+function showBox(index){
+    for (var i = 0; i < boxes.length; i++) {
+        if (i == index) {
+            boxes[i].style.display = "block";
+        } else {
+            boxes[i].style.display = "none";
+        }
     }
 }
+
