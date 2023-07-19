33ec1466ff9fb64e88c26b685a9233962be7aaa5
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2023-07-19 17:10:19 -0300

    atualizac ao

--- javascript/javascript-2.js
+++ javascript/javascript-2.js
@@ -1,4 +1,14 @@
 function changeDiv(opcao) {
+  var divs = document.querySelectorAll('div[id^="div"]');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].classList.add('hide');
+  }
+  document.getElementById('div' + opcao).classList.remove('hide');
+}
+
+
+
+/*function changeDiv(opcao) {
       // Esconder todas as divs
       var divs = document.getElementsByClassName("hide");
       for (var i = 0; i < divs.length; i++) {
@@ -8,4 +18,4 @@ function changeDiv(opcao) {
       // Mostrar a div selecionada
       var divSelecionada = document.getElementById("div" + opcao);
       divSelecionada.style.display = "block";
-    }
+    } */ 
